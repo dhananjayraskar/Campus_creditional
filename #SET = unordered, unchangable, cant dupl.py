53d644bd12bcc375fdc6de4feb69d303c6ae84a2
Dhananjay Govindrao Raskar <57898900+dhananjayraskar@users.noreply.github.com>
2022-05-27 17:27:37 +0530

    set in python
    and dict

---
+++ #SET = unordered, unchangable, cant dupl.py
@@ -0,0 +1,32 @@
+#SET = unordered, unchangable, cant duplicate
+
+# Cities = set(("Mumbai", "Chennai", "Delhi",50, 50.56, True, False))
+# print(Cities)
+
+# print(type(Cities))
+# print(len(Cities))
+
+Cities = {"Mumbai" , "Chennai" , "Delhi" , "Chennai"}
+# for x in Cities:
+#     print(x)
+
+# print("Agra" in Cities)
+
+#Cities = {"Mumbai" , "Chennai" , "Delhi" , "Chennai"}
+# Cities.add("Haryana")
+# print(Cities)
+
+#countries =("India" , "US" , "UK")
+#Cities.update(countries)
+#print(Cities)
+
+Cities = {"Mumbai" , "Chennai" , "Delhi" , "Chennai"}
+print(Cities)
+
+a =Cities.pop()
+print(a)
+
+#Cities.remove("Mumbai")
+#print(Cities)
+
+
---
+++ Dictionry.py
@@ -0,0 +1,16 @@
+# DICTIONARIES = KEY>VALUE
+# ordered, changable, duplication not allowed
+
+newdictionry = {"Name":"Dhananjay"
+                "RollNo":"311042"
+                "Age":"20"
+ }
+ newdictionry["BirthMonth"] = "May"
+ 
+print(newdictionry)
+
+#print(newdictionry["RollNo"])
+
+#print(type(newdictionry))
+
+#print(len(newdictionry))
