35c0270529a068fa6c1e9133350fa40404241605
Dhananjay Govindrao Raskar <57898900+dhananjayraskar@users.noreply.github.com>
2022-05-28 10:21:39 +0530

    dictionry in py

---
+++ dict_1 = {.py
@@ -0,0 +1,28 @@
+#dict_1 = {
+ #   "name" : "Harry",
+ #   "age" : 15,
+  #  "gender" : "male"
+#}
+#print(dict_1.items())
+
+
+
+
+#data = {
+ #   "name" : "Harry",
+  #  "company_name":"JBK",
+  #  "age": 25
+#}
+#print(data.get("name"))
+#print(data)
+#print(data.pop("age"))
+#print(data)
+
+set1 = {5,9,7,5,11,3,1,6,10,1}
+#print(len(set1))
+print((set1.add("99")))
+#copy_set = set1.copy()
+#print(copy_set)
+print(set1.pop())
+print(set1.remove(11))
+print(set1)
