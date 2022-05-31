3daba3e9ab33c0c680b597d89eb21d23b4af2b3e
Dhananjay Govindrao Raskar <57898900+dhananjayraskar@users.noreply.github.com>
2022-05-31 11:03:26 +0530

    for loop
    .

---
+++ loop94.py
@@ -0,0 +1,89 @@
+#loop94
+#While Loop
+#while condition
+    #if true execute this code
+
+
+
+
+#i = 1
+#while i<20:
+    #print(i)
+     
+    #if i == 14:
+        #break
+    #i += 2    #i=i+1
+
+
+
+
+#i = 25
+#while i<30:
+    
+     #i += 1    #i=i+1
+     #if i==27:
+        #continue
+     #print(i)
+
+
+#i = 1
+#while i<10:
+  #  print(i)
+   # i += 1
+#else:
+   # print("i is more than 9")
+
+
+
+    #'''write a program to print the following using while loop
+    #first 10 even, odd, natural number'''
+
+# for even number
+i = 2
+while i<=20:
+    print(i)
+    i+=2
+
+#for odd number
+i=1
+while i<=20:
+    print(i)
+    i+=2
+
+#natural number
+i=1
+while i<=10:
+    print(i)
+    i=i+1
+
+   
+
+
+#i=1
+#even =[]
+#odd =[]
+#natural =[]
+#while i<=10:
+ #   natural.append(i)
+  #  if i%2 == 0:
+  #      even.append(i)
+   # else:
+   #     odd.append(i)
+    #    i+=1
+
+#print("Even Numbers:", even)
+#print("Odd Numbers:",odd)
+#print("Natural Number:", natural)
+
+
+
+
+    
+     
+
+    
+
+    
+    
+
+
