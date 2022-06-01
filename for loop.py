492b328ef631c0e34033c62fb0cf4a9e43db302b
Dhananjay Govindrao Raskar <57898900+dhananjayraskar@users.noreply.github.com>
2022-06-01 10:58:04 +0530

    for loop
    range in for loop
    fibonacci series using for

---
+++ for loop.py
@@ -0,0 +1,77 @@
+#FOR LOOP
+
+
+#Cities = ["Mumbai", "Chennai", "Delhi"]
+#for x in Cities:
+ #   print(x)
+
+
+#for x in "Cities":
+ #   print(x)
+
+
+#RANGE in for loop
+#for x in range(5):
+  #  print(x)
+
+#for x in range(10,20):       #increament by 1
+  #  print(x)
+
+#for x in range(10,20,2):      #increament by 2
+ #   print(x)
+
+
+
+#how the FOR-ELSE loop works
+#for x in range(10,20):
+  #  print(x)
+#else:
+  #  print("Done")
+
+'''Display the fibanacci series up to 10 tems
+The fibanacci sequence is a series of number.
+The next number is found by adding up the two number before it.
+The first two number are 0 1nd 1.
+
+
+For example  0,1,1,2,3,5,8,13,21,...
+The next number is the sum of 13+21=34 '''
+
+
+# Program to display the Fibonacci sequence up to n-th term
+
+#nterms = int(input("How many terms? "))
+
+# first two terms
+#n1, n2 = 0, 1
+#count = 0
+
+# check if the number of terms is valid
+#if nterms <= 0:
+   #print("Please enter a positive integer")
+# if there is only one term, return n1
+#elif nterms == 1:
+  # print("Fibonacci sequence upto",nterms,":")
+   #print(n1)
+# generate fibonacci sequence
+#else:
+  # print("Fibonacci sequence:")
+   #while count < nterms:
+      # print(n1)
+       #nth = n1 + n2
+       # update values
+       #n1 = n2
+       #n2 = nth
+       #count += 1
+
+
+
+
+
+
+
+
+
+
+
+
