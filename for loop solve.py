75a7eea4d3eb26ec94f70ec882c45d3877aa5256
Dhananjay Govindrao Raskar <57898900+dhananjayraskar@users.noreply.github.com>
2022-06-03 11:05:02 +0530

    for loop solve problem

---
+++ for loop solve.py
@@ -0,0 +1,50 @@
+'''Questions on for loop'''
+#Q1.
+
+
+#number = int(input("Number: "))
+#sum = 0
+#for i in range(1, number + 1):
+ #   sum += i
+#print(sum)
+
+
+#Q2.
+
+#num = [22, 175, 50, 220, 145, 935, 40]
+#for item in num:
+  #  if item > 500:
+   #     break
+    #elif item > 150:
+     #  continue
+     #check divisible by 5
+    #elif item % 5 == 0:
+      #  print(item)
+
+
+#Q3.
+#num = 100000
+#count = 0
+
+#while num != 0:
+  #  num //= 10
+  #  count += 1
+
+#print("Total digits: " + str(count))
+
+#Q4.
+#list1 = [10, 20, 30, 40, 50]
+
+#for i in range(len(list1) - 1,-1,-1) :
+  #  print(list1[i])
+
+ #Q5.
+
+my_list = [10,20,30,40,50,60,70,80,90,100]
+for i in my_list[1::2]:
+    print(i, end=" ") 
+    
+
+
+
+
