8b2a92cbee168cf33ed50fa139d20e39ddcc76f7
Dhananjay Govindrao Raskar <57898900+dhananjayraskar@users.noreply.github.com>
2022-06-08 21:39:26 +0530

    functions in python
    ..

---
+++ function solve.py
@@ -0,0 +1,25 @@
+# lamda map filter
+
+#Q1.
+#minutes = int(input("Input minutes: ")) * 60
+
+ 
+#Totaltime =  minutes 
+ 
+#print("The  amounts of seconds", Totaltime)
+
+#Q3.
+
+#def test_number3(x, y):
+ #  if (x+y)<=100:
+  #     return True
+  # else:
+      # return False
+
+#print(test_number3(5, 57))
+#print(test_number3(77, 30))
+#print(test_number3(100, 0))
+
+#Q4.
+
+
